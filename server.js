2899e34d56a267909ec3e6b6c0124141dffd9da5
Nurhadihdgy <teks0754@gmail.com>
2026-01-25 18:50:47 +0700

    fix: cors allowed resource

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.use(cors({
   origin: process.env.CLIENT_URL,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  allowedHeaders: ["Content-Type", "Authorization"],
+  exposedHeaders: ["Content-Type"]
 }));
 
 
