f7702101ee689d958ab40e1f5992d2c00f6ced68
Nurhadihdgy <teks0754@gmail.com>
2026-01-25 16:18:30 +0700

    fix:cors

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors({ origin: process.env.CLIENT_URL || "*" }));
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
